27e9a84d751aa96a3059033340f1772260d9dcc2
jiyong LEE <jyman1995@gmail.com>
2021-09-30 23:18:15 +0900

    Server Setup
    Express, pug, babel, nodemon 셋업

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+// express view engine 설정
+app.set("view engine", "pug");
+
+// views template 경로 설정
+//__dirname : 현재 server.js 경로
+app.set("views", __dirname + "/views");
+// image, css, Js 같은 정적파일 제공
+// /public url로 이동시 /pubilc내의 디렉토리를 볼 수 있음.
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+const handleListen = () => console.log("Listening on http://localhost:5000");
+app.listen(5000, handleListen);
