d7c3517e2617acee1208e048ddc51e61aca28540
jiyong LEE <jyman1995@gmail.com>
2021-10-01 16:32:22 +0900

    Create http + WebSoket

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -12,5 +14,11 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log("Listening on http://localhost:5000");
-app.listen(5000, handleListen);
+
+// http + websocket 서버 만들기
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(5000, handleListen);
