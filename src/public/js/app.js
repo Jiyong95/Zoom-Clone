46bf8c77cc7f6ec4944b51a6a3a4ce82a6153855
jiyong LEE <jyman1995@gmail.com>
2021-10-02 22:46:45 +0900

    WebSocket send Message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,19 @@
 // 서버와 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server✅");
+});
+
+// Server에서 보낸 message를 받음.
+socket.addEventListener("message", (message) => {
+  console.log("New message : ", message.data, "from the server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("Connected to Server❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the brower!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -21,10 +21,18 @@ const handleListen = () => console.log("Listening on http://localhost:5000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-// event가 발생하길 기다리는 함수. + 연결된 서버 정보를 soket으로 콜백 함수에 전달
-wss.on("connection", handleConnection);
+// Server에서 Browser로 연결, Browser 실행 시 호출
+wss.on("connection", (socket) => {
+  // event가 발생하길 기다리는 함수. + 연결된 브라우저 정보를 socket으로 콜백 함수에 전달
+  // console.log(socket);
+  console.log("Connected to Browser✅");
+  socket.on("close", () => console.log("Disconnected from the Browser❌"));
+  // Browser(app.js)에서 Server로 보낸 메세지를 받음
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+
+  socket.send("hello!");
+});
 
 server.listen(5000, handleListen);
